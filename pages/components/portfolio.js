4df5745d8105013e23ede49ff4fb52450bc4c22d
YMLeonardS <YMLeonardS@users.noreply.github.com>
2023-10-31 15:24:36 +0700

    Update portfolio.js

--- pages/components/portfolio.js
+++ pages/components/portfolio.js
@@ -8,8 +8,8 @@ export default function Portfolio() {
           <h4 className="font-semibold text-lg text-primary mb-2">Portfolio</h4>
           <h2 className="font-bold text-dark text-3xl mb-4 sm:text-4xl lg:text-4xl">Last Project</h2>
           <p className="font-medium text-secondary md:text-lg">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis facere eos dolorem expedita eligendi, corrupti architecto quae. Vel,
-            fugit hic!
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis facere eos dolorem expedita eligendi,
+            corrupti architecto quae. Vel, fugit hic!
           </p>
         </div>
       </div>
@@ -48,6 +48,97 @@ export default function Portfolio() {
         <div className="mb-12 p-4 md:w-1/2">
           <div className="rounded-md shadow-md overflow-hidden">
             <Image src="/portfolio/4.png" width={1000} height={1000} alt="CS Core Performance" />
+          </div>
+          <h3 className="font-semibold text-xl text-dark mt-5 mb-3">CS Core Performance</h3>
+          <p className="font-medium text-base text-secondary">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius asperiores temporibus rem?
+          </p>
+        </div>
+
+        <div className="mb-12 p-4 md:w-1/2">
+          <div className="rounded-md shadow-md overflow-hidden">
+            <Image src="/portfolio/A.png" width={1000} height={1000} alt="CS Core Performance" />
+          </div>
+          <h3 className="font-semibold text-xl text-dark mt-5 mb-3">CS Core Performance</h3>
+          <p className="font-medium text-base text-secondary">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius asperiores temporibus rem?
+          </p>
+        </div>
+        <div className="mb-12 p-4 md:w-1/2">
+          <div className="rounded-md shadow-md overflow-hidden">
+            <Image src="/portfolio/B.png" width={1000} height={1000} alt="CS Core Performance" />
+          </div>
+          <h3 className="font-semibold text-xl text-dark mt-5 mb-3">CS Core Performance</h3>
+          <p className="font-medium text-base text-secondary">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius asperiores temporibus rem?
+          </p>
+        </div>
+        <div className="mb-12 p-4 md:w-1/2">
+          <div className="rounded-md shadow-md overflow-hidden">
+            <Image src="/portfolio/C.png" width={1000} height={1000} alt="CS Core Performance" />
+          </div>
+          <h3 className="font-semibold text-xl text-dark mt-5 mb-3">CS Core Performance</h3>
+          <p className="font-medium text-base text-secondary">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius asperiores temporibus rem?
+          </p>
+        </div>
+        <div className="mb-12 p-4 md:w-1/2">
+          <div className="rounded-md shadow-md overflow-hidden">
+            <Image src="/portfolio/D.png" width={1000} height={1000} alt="CS Core Performance" />
+          </div>
+          <h3 className="font-semibold text-xl text-dark mt-5 mb-3">CS Core Performance</h3>
+          <p className="font-medium text-base text-secondary">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius asperiores temporibus rem?
+          </p>
+        </div>
+        <div className="mb-12 p-4 md:w-1/2">
+          <div className="rounded-md shadow-md overflow-hidden">
+            <Image src="/portfolio/E.png" width={1000} height={1000} alt="CS Core Performance" />
+          </div>
+          <h3 className="font-semibold text-xl text-dark mt-5 mb-3">CS Core Performance</h3>
+          <p className="font-medium text-base text-secondary">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius asperiores temporibus rem?
+          </p>
+        </div>
+        <div className="mb-12 p-4 md:w-1/2">
+          <div className="rounded-md shadow-md overflow-hidden">
+            <Image src="/portfolio/F.png" width={1000} height={1000} alt="CS Core Performance" />
+          </div>
+          <h3 className="font-semibold text-xl text-dark mt-5 mb-3">CS Core Performance</h3>
+          <p className="font-medium text-base text-secondary">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius asperiores temporibus rem?
+          </p>
+        </div>
+        <div className="mb-12 p-4 md:w-1/2">
+          <div className="rounded-md shadow-md overflow-hidden">
+            <Image src="/portfolio/G.png" width={1000} height={1000} alt="CS Core Performance" />
+          </div>
+          <h3 className="font-semibold text-xl text-dark mt-5 mb-3">CS Core Performance</h3>
+          <p className="font-medium text-base text-secondary">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius asperiores temporibus rem?
+          </p>
+        </div>
+        <div className="mb-12 p-4 md:w-1/2">
+          <div className="rounded-md shadow-md overflow-hidden">
+            <Image src="/portfolio/H.png" width={1000} height={1000} alt="CS Core Performance" />
+          </div>
+          <h3 className="font-semibold text-xl text-dark mt-5 mb-3">CS Core Performance</h3>
+          <p className="font-medium text-base text-secondary">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius asperiores temporibus rem?
+          </p>
+        </div>
+        <div className="mb-12 p-4 md:w-1/2">
+          <div className="rounded-md shadow-md overflow-hidden">
+            <Image src="/portfolio/I.png" width={1000} height={1000} alt="CS Core Performance" />
+          </div>
+          <h3 className="font-semibold text-xl text-dark mt-5 mb-3">CS Core Performance</h3>
+          <p className="font-medium text-base text-secondary">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius asperiores temporibus rem?
+          </p>
+        </div>
+        <div className="mb-12 p-4 md:w-1/2">
+          <div className="rounded-md shadow-md overflow-hidden">
+            <Image src="/portfolio/J.png" width={1000} height={1000} alt="CS Core Performance" />
           </div>
           <h3 className="font-semibold text-xl text-dark mt-5 mb-3">CS Core Performance</h3>
           <p className="font-medium text-base text-secondary">
